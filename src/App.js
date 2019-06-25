98b015ea96f2d803984852d59aac414ddaf66948
maxlvl <maximvanlyl@gmail.com>
2019-06-24 23:31:09 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          TO_DO LIST
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
